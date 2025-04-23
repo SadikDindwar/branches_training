e61ecd7cba6c17a9305006a2fe46ee5678836f1a
Sadik B <sadikdindwar@gmail.com>
2025-04-23 21:56:10 +0530

    first push

---
+++ src/main/java/com/sadik/BranchesInGitApplication.java
@@ -0,0 +1,13 @@
+package com.sadik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchesInGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchesInGitApplication.class, args);
+	}
+
+}
